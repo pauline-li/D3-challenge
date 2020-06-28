ba9cf3b9791ee10010e71ac22029ff682ede8371
Pauline Li <pli@teloslogistics.local>
2020-06-28 14:35:08 -0500

    Add files

--- D3_data_journalism/assets/js/d3_app_old_version.js
+++ D3_data_journalism/assets/js/d3_app_old_version.js
@@ -19,11 +19,7 @@ var svg = d3.select("#scatter") // insert chart to tag id "scatter"
   .attr("height", svgHeight);
 
 var chartGroup = svg.append("g") // group, g element
-    .attr("height", height)
-    .attr("width", width)
-    .attr("transform", `translate(${margin.left}, ${margin.top})`); // purpose of transform is to move screen
-    // translate is the....transform = "translate(x, y)"     
-
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Import Data
 d3.csv("assets/data/data.csv").then(function(censusData) {
@@ -41,27 +37,11 @@ d3.csv("assets/data/data.csv").then(function(censusData) {
     // Step 2: Create scale functions
     // ==============================
     var xLinearScale = d3.scaleLinear()
-      .domain(
-          [
-              d3.min(censusData, d => d.poverty)  * 0.8,
-              d3.max(censusData, d => d.poverty)  * 1.1     // up to 24       
-              
-              
-          ])  
-    // x LINEAR scale for scatter plots..where data coming in
-    //   .domain([8, d3.max(censusData, d => d.poverty)])  ***work
+      .domain([8, d3.max(censusData, d => d.poverty)])  // x LINEAR scale for scatter plots
       .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
-      .domain(
-        [
-        d3.min(censusData, d => d.healthcare)  * 0.8,
-        d3.max(censusData, d => d.healthcare)  * 1.1      // up to 28       
-        
-        
-        ]) 
-    //   .domain(d3.extent(censusData, d => d.healthcare)) //y LINEAR scale for scatter plots
-    //   .domain([0, d3.max(censusData, d => d.healthcare)]   ***work
+      .domain([0, d3.max(censusData, d => d.healthcare)]) //y LINEAR scale for scatter plots
       .range([height, 0]);
 
     // Step 3: Create axis functions
@@ -84,10 +64,10 @@ d3.csv("assets/data/data.csv").then(function(censusData) {
     .data(censusData)
     .enter()
     .append("circle")
-    .attr("cx", d => xLinearScale(d.poverty)) // look at this cx (center x)data point / hair_lenght property and apply return that x coordinate
+    .attr("cx", d => xLinearScale(d.poverty)) // look at this data point / hair_lenght property and apply return that x coordinate
     .attr("cy", d => yLinearScale(d.healthcare)) // similiar to above except epply to y coordinate
-    .attr("r", "12") // radius
-    .attr("class","stateCircle") //circle color styling on d3Style.css, class .stateCircle
+    .attr("r", "11")
+    .attr("fill", "lightblue")
     .attr("opacity", ".9");
 
     //============Add Test to Circle
@@ -99,9 +79,10 @@ d3.csv("assets/data/data.csv").then(function(censusData) {
     .text(d => d.abbr)
     .attr("x", d => xLinearScale(d.poverty)) // look at this data point / poverty property and apply return that x coordinate
     .attr("y", d => yLinearScale(d.healthcare))
-    .attr("class","stateText") // .stateText class in d3Style.css stying for circle text
+    .classed("stateText", true)
     .attr("font-size","9");
-  
+    // .classed("stateText", true)
+    // .attr("fill", "black");
 
     console.log(censusData); //*************** */
 
@@ -134,12 +115,12 @@ d3.csv("assets/data/data.csv").then(function(censusData) {
       .attr("y", 0 - margin.left + 40) // subtracting margin left and add 40, 40 pixels away from the edge
       .attr("x", 0 - (height / 2))
       .attr("dy", "1em") // moving the text by 1em...centering
-      .attr("class", "axisText") // class is axisText
+    //   .attr("class", "axisText") // class is axisText
       .text("Lack Healthcare (%)");
 
     chartGroup.append("text")
       .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
-      .attr("class", "axisText") // axisText is in css...us attribute fill to change color of text...example fill: #55aaf...no line breaking
+      .attr("class", "aText") // aText is in css...us attribute fill to change color of text...example fill: #55aaf...no line breaking
       .text("In Poverty (%)"); // append to text element....
   }).catch(function(error) {
     console.log(error);
